3b0d81b8afe45b37005df0eba6e3670d07aeb4a5
kb1etr <schuett@hartford.edu>
2025-09-14 19:39:52 -0400

    Modified initials and updated string message

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -8,11 +8,11 @@ public class Assignment1Program1 {
     System.out.println("LL                SSS");
     System.out.println("LL              SS");
     System.out.println("LL             SS");
-    System.out.println("LLLLLLLLL   SSSSSSS");
+    System.out.println("LLLLLLLLL  SSSSSSS");
   
   // 
   
-    String message;
+    String string;
     Scanner string = new Scanner(System.in);
 
     System.out.print("Please enter a 5-character string:");
