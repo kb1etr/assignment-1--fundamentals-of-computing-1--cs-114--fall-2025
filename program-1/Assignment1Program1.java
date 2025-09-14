f106f5bf6f18f1afbe773b8b55da33a1b408ce55
kb1etr <schuett@hartford.edu>
2025-09-14 19:38:40 -0400

    add string

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Assignment1Program1 {
   public static void main(String[] args) {
     System.out.println("LL            SSSSSSS");
@@ -7,4 +9,13 @@ public class Assignment1Program1 {
     System.out.println("LL              SS");
     System.out.println("LL             SS");
     System.out.println("LLLLLLLLL   SSSSSSS");
+  
+  // 
+  
+    String message;
+    Scanner string = new Scanner(System.in);
+
+    System.out.print("Please enter a 5-character string:");
+    message = string.nextLine();
+  
   }}
