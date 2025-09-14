72a89decf4f191ea60774e932a030efd6cc3dd96
kb1etr <schuett@hartford.edu>
2025-09-14 19:28:55 -0400

    Worked on initial project

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,10 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
-
-  }
-}
+    System.out.println("LL            SSSSSSS");
+    System.out.println("LL            SS");
+    System.out.println("LL              SS");
+    System.out.println("LL                SSS");
+    System.out.println("LL              SS");
+    System.out.println("LL             SS");
+    System.out.println("LLLLLLLLL   SSSSSSS");
+  }}
