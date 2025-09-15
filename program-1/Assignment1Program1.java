844ff3fa78bb03d71f932ca6562339624de0ce1e
kb1etr <schuett@hartford.edu>
2025-09-14 20:45:56 -0400

    Fixed the initials

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class Assignment1Program1 {
   public static void main(String[] args) {
-    System.out.println("LL            SSSSSSS");
-    System.out.println("LL            SS");
+    System.out.println("LL                 SSSSSSSSSSSS");
+    System.out.println("LL                SSSSSSSSSSSSSS");
+    System.out.println("LL               SS");
     System.out.println("LL              SS");
-    System.out.println("LL                SSS");
-    System.out.println("LL              SS");
-    System.out.println("LL             SS");
-    System.out.println("LLLLLLLLL  SSSSSSS");
-  
+    System.out.println("LL               SS");
+    System.out.println("LL                 SSSSSSSSS");
+    System.out.println("LL                 SSSSSSSS");
+    System.out.println("LL                          SS");
+    System.out.println("LL                           SS");
+    System.out.println("LL                          SS");
+    System.out.println("LLLLLLLLLLLL     SSSSSSSSSSSSS");
+    System.out.println("LLLLLLLLLLLL      SSSSSSSSSSS");
   // Please enter a number in Fahrenheit:
   
     String string;
