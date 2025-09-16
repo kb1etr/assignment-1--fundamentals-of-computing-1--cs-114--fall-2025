9094e454920019edbd3c5f01a65112c1f439063f
kb1etr <schuett@hartford.edu>
2025-09-16 09:54:55 -0400

    Strength updated initials

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -8,25 +8,23 @@ public class Assignment1Program1 {
     System.out.println("LL              SS");
     System.out.println("LL               SS");
     System.out.println("LL                 SSSSSSSSS");
-    System.out.println("LL                 SSSSSSSS");
-    System.out.println("LL                          SS");
+    System.out.println("LL                 SSSSSSSSS");
     System.out.println("LL                           SS");
-    System.out.println("LL                          SS");
+    System.out.println("LL                            SS");
+    System.out.println("LL                           SS");
     System.out.println("LLLLLLLLLLLL     SSSSSSSSSSSSS");
     System.out.println("LLLLLLLLLLLL      SSSSSSSSSSS");
   // Please enter a number in Fahrenheit:
   
-    String string;
+    String Message;
     String tempF;
     
     Scanner string = new Scanner(System.in);
 
-    System.out.println("Please enter a 5-character string:");
-    message = string.nextLine();
-
-    Scanner tempF = new Scanner(System.in);
+    System.out.print("Please enter a 5-character string:");
+    Message = string.nextLine();
     
-    System.out.println("Please enter a number in Fahrenheit:");
-    message = tempF.nextLine();
+    System.out.print("Please enter a number in Fahrenheit:");
+    tempF = string.nextLine();
   
   }}
