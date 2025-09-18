aa698b494cdd2ac8f4fee805aa7240860ec5c4d5
kb1etr <schuett@hartford.edu>
2025-09-18 14:18:50 -0400

    Started program two

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Assignment1Program2 {
   public static void main(String[] args) {
+  // Scanner keyInput = new Scanner(System.in);
+  //     tempF = keyInput.nextDouble();
+
+ // Base Conversion Program
+
+//Please enter a base (2 – 9): 3
+
+//The maximum, 4-digit, base 10 number in base 3 is 80.
+//Now, enter a base 10 number in the range 0 to 80 to convert: 17
+
+//17 (base 10) = 0122 (base 3)
+
+
+
+
+
 
   }
+
 }
