43cb9fb97a78de735f80d1a0fff4ceac84bb5fa8
kb1etr <schuett@hartford.edu>
2025-10-07 08:50:40 -0400

    Update Assignment1Program2.java

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+
 public class Assignment1Program2 {
   public static void main(String[] args) {
   int inputBase, inputBaseTen, maxNub; 
-  int base1, modbase1, base2, modbase2, base3, modbase3, modbase4;
+  int base1, modBase1, base2, modBase2, base3, modBase3, modBase4;
   Scanner keyInput = new Scanner(System.in);
   System.out.println("Base Conversion Program");
   System.out.println();
@@ -14,13 +15,13 @@ public class Assignment1Program2 {
   System.out.print("Now, enter a base 10 number in the range 0 to " + maxNub + " to convert: ");
   inputBaseTen = keyInput.nextInt();
   base1=(inputBaseTen/inputBase);
-  modbase1=(inputBaseTen%inputBase);
+  modBase1=(inputBaseTen%inputBase);
   base2=(base1/inputBase);
-  modbase2=(base1%inputBase);
+  modBase2=(base1%inputBase);
   base3=(base2/inputBase);
-  modbase3=(base2%inputBase);
-  modbase4=(base3%inputBase);
+  modBase3=(base2%inputBase);
+  modBase4=(base3%inputBase);
   System.out.println();
-  System.out.println(inputBaseTen + " (base 10) = " + modbase4 + modbase3 + modbase2 + modbase1 + " (base " + inputBase + ")");
+  System.out.println(inputBaseTen + " (base 10) = " + modbase4 + modBase3 + modBase2 + modBase1 + " (base " + inputBase + ")");
   }
 }
